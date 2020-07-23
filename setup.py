2c45546ca3d8813b5de23f465934a8cfbdbe2405
AndrewKing <11490628+andrew000@users.noreply.github.com>
2020-07-23 14:19:11 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cow-scheduler',
+    version='0.0.1',
+    packages=['scheduler'],
+    url='https://github.com/andrew000/cow-scheduler',
+    license='GNU General Public License v3.0',
+    author='AndrewKing',
+    author_email='',
+    description='Run func later)'
+)
